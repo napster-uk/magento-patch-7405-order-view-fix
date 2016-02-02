5cd1b4c876d3e29c1d3e68380df101934fb0610f
Sofiane <sofianek94@gmail.com>
2016-02-02 12:44:10 +0000

    adding module folders

---
+++ app/code/local/Sofiane/Order/Helper/Data.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by Sofiane Khelfaoui
+ * User: Sofiane
+ * Email: sofianek94@gmail.com
+ * Date: 02/02/2016
+ * Time: 11:25
+ */
+class Sofiane_Order_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
+
